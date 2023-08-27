3c9fbfb51ec2025ed7035fc5f0cbb2172f9ec440
Jakub517 <pavlu.jakub8@seznam.cz>
2023-08-27 13:39:31 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+function checkLogin() {
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+
+    // Example of checking username and password
+    if (username === 'cert' && password === 'tajneHeslo') {
+        // Successful login
+        Swal.fire({
+            icon: 'success',
+            title: 'Úspěch',
+            text: 'Úspěšně jste se přihlásili.',
+            confirmButtonText: 'Přejít na stránku',
+        }).then((result) => {
+            if (result.isConfirmed) {
+                window.location.href = 'jakub517.github.io/cert'; // Nahraďte URL adresou cílové stránky
+            }
+        });
+    } else {
+        // Failed login
+        Swal.fire({
+            icon: 'error',
+            title: 'Chyba',
+            text: 'Zadali jste nesprávné údaje. Zkontrolujte své jméno a heslo.',
+        });
+    }
+}
